35471f3ece1f8a0e2e3bbbaf65c6d6f2ae286ed8
Katie Breuil <kcbreuil@gmail.com>
2020-05-28 18:29:21 -0400

    exercise tracker backend setup

---
+++ client/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return <div className="container">Hello World</div>;
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,61 @@
+/* these lines load the libraries and create variables that give us access to all
+of the methods that are built into each library */
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+/* loads the dotenv library and calls the config() method, which loads the 
+variables declared in the .env file into process.env */
+require("dotenv").config();
+
+/* calls the express() function from the express library, creating your 
+Express app */
+
+const app = express();
+
+/* declares the port as either whatever it is saved as within the .evn file, 
+or otherwise defaults to port 5000 */
+const port = process.env.PORT || 5000;
+
+/* enables the Express server to respond to requests, makes your server 
+accessible to any domain that requests a resource from your server via a browser */
+
+app.use(cors());
+
+/* a method that is built into Express to recognize the incoming requests 
+as JSON Objects. This method is called as a middleware in your application, 
+which means it is a method/function/operation that is called between processing
+an incoming request and returning a response */
+app.use(express.json());
+
+// what does this do?
+const uri = process.env.ATLAS_URI;
+
+/* tells your Mongoose library where your database lives and avoids 
+deprecation warning errors */
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+/* accesses the .connection method within the Mongoose library to make a 
+connection to your database */
+const connection = mongoose.connection;
+
+// what does this do?
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+const exercisesRouter = require("./backend/routes/exercises");
+const usersRouter = require("./backend/routes/users");
+
+app.use("/exercises", exercisesRouter);
+app.use("/users", usersRouter);
+
+// what does this do?
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
